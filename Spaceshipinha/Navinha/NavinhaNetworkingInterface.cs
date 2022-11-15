dd69c360d24994a801a4a1f569bb6fa4724bd05b
Locochoco <ivanrwpf@gmail.com>
2022-11-14 22:21:13 -0300

    Removed comments

--- Spaceshipinha/Navinha/NavinhaNetworkingInterface.cs
+++ Spaceshipinha/Navinha/NavinhaNetworkingInterface.cs
@@ -5,8 +5,6 @@ namespace Spaceshipinha.Navinha
 {
     public class NavinhaNetworkingInterface : SimpleNetworkingInterface
     {
-        //public override bool IsPuppet { get => isPuppet; set => isPuppet = value; }
-
         public NaveThrusterFlameController naveThrusterFlameController;
         public NaveThrusterController naveThrusterController;
 
@@ -21,26 +19,5 @@ namespace Spaceshipinha.Navinha
             naveThrusterFlameController.IsPuppet(isPuppet);
             base.OnIsPuppetChange(isPuppet);
         }
-        //public void Start()
-        //{
-
-        //    if (IsPuppet)
-        //    {
-        //        for (int i = 0; i < scriptsToDisableWhenPuppet.Length; i++)
-        //        {
-        //            scriptsToDisableWhenPuppet[i].enabled = false;
-        //        }
-        //        for (int i = 0; i < gameObjectsToDisableWhenPuppet.Length; i++)
-        //        {
-        //            gameObjectsToDisableWhenPuppet[i].SetActive(false);
-        //        }
-
-        //        Rigidbody r = GetComponent<Rigidbody>();
-        //        if (r != null)
-        //        {
-        //            r.isKinematic = RigidbodyToKinematicWhenPuppet;
-        //        }
-        //    }
-        //}
     }
 }
